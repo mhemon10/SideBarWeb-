f2699d9eb7a6fb5ffc1c01d43abbe63989b6b075
MH Emon <mhemon@MHs-MacBook-Air.local>
2025-11-06 16:14:59 +0600

    my second commit

--- sidebar-menu/src/components/Nav/Navbar.jsx
+++ sidebar-menu/src/components/Nav/Navbar.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import { BsArrowLeft } from "react-icons/bs";
 
 const Navbar = () => {
-    return (
-        <div className='flex'>
-            <div className="bg-indigo-950 h-screen p-5 pt-8 w-72">Sidebar</div>
-            <div className='p-7'>
-<h1 className='text-2xl font-semibold'> Home Page</h1>
-            </div>
+  const [open, setOpen] = useState(true);
 
-        </div>
-        
-    );
+  return (
+    <div className="flex">
+      <div
+        className={`bg-indigo-950 h-screen p-5 pt-8 relative transition-all duration-300 ${
+          open ? "w-72" : "w-20"
+        }`}
+      >
+        <BsArrowLeft
+          className={`bg-white text-indigo-950 text-3xl rounded-full absolute -right-3 top-9 border border-indigo-950 cursor-pointer transition-transform duration-300 ${
+            !open && "rotate-180"
+          }`}
+          onClick={() => setOpen(!open)}
+        />
+      </div>
+
+      <div className="p-7 flex-1">
+        <h1 className="text-2xl font-semibold text-black">Home Page</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
